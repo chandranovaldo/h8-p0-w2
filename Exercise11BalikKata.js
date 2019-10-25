43c42bc7cbbb9ee6b409b6938a285ef3b4458bb9
chandranovaldo <chandra_novaldo@hotmail.com>
2019-10-25 15:21:34 +0700

    editExercise11

--- Exercise11BalikKata.js
+++ Exercise11BalikKata.js
@@ -1,20 +1,18 @@
+//Exercise 11
 function balikKata(kata) {
-    //memisahkan kata satu persatu jadi array
-    var pisahKata = kata.split(""); 
- 
-    //membalikan array yang dibuat 
-    var balikinArray = pisahKata.reverse();
- 
-   //menggabungkan setiap element array jadi string
-    var gabungArray = balikinArray.join(""); 
-
-    //mengembalikan hasil penggabungan
-    return gabungArray;
-}
-
-// TEST CASES
-console.log(balikKata('Hello World and Coders')); // sredoC dna dlroW olleH
-console.log(balikKata('John Doe')); // eoD nhoJ
-console.log(balikKata('I am a bookworm')); // mrowkoob a ma I
-console.log(balikKata('Coding is my hobby')); // ybboh ym si gnidoC
-console.log(balikKata('Super')); // repuS
+    
+    var kataBalik ='';
+  
+    for (i=kata.length -1; i>=0; i--)
+    {
+      kataBalik = kataBalik + kata[i];
+    }
+    return kataBalik;
+  }
+  
+  // TEST CASES
+  console.log(balikKata('Hello World and Coders')); // sredoC dna dlroW olleH
+  console.log(balikKata('John Doe')); // eoD nhoJ
+  console.log(balikKata('I am a bookworm')); // mrowkoob a ma I
+  console.log(balikKata('Coding is my hobby')); // ybboh ym si gnidoC
+  console.log(balikKata('Super')); // repuS
